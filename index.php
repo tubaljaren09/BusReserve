e667c003c9a4d4f40aaf78b36bdcd95f85abf241
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2021-02-11 12:43:00 +0800

    Add grid to main + css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+<body>
+    <div class="main">
+        <div class="main-top">
+
+        </div>
+        <div class="main-middle">
+
+        </div>
+        <div class="main-bottom">
+
+        </div>
+    </div>
+</body>
+</html>
