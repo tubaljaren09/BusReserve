4fcaeaa95a145c31c118be7237147eac19d6f867
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2021-02-11 13:45:31 +0800

    Add navbar + css

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
 <body>
     <div class="main">
         <div class="main-top">
-
+            <div class="container-top">
+                <div class="top">
+                    <div class="content">
+                        <h1>BUS TICKET RESERVATION</h1>
+                        <h3>WITH MEDICAL APPREHENSION</h3>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="content">
+                        <ul>
+                            <li><a class="active" href="index.php">HOMEPAGE</a></li>
+                            <li><a href="#">BOOK TICKET</a></li>
+                            <li><a href="#">ABOUT US</a></li>
+                            <li><a href="#">ABOUT BUS</a></li>
+                            <li><a href="#">ADMIN</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="main-middle">
 
