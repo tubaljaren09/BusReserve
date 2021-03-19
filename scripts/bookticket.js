e86e6a309d53cd85313fbc21894acfeeedd579ef
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2021-03-19 16:26:15 +0800

    Add close modal by clicking outside

--- scripts/bookticket.js
+++ scripts/bookticket.js
@@ -11,6 +11,13 @@ closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 })
 
+// Close modal without click x button
+window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display = "none";
+    }
+}
+
 if(window.history.replaceState){
     window.history.replaceState(null, null, window.location.href);
 }
